782459c0c7f029096301598f24cc1c3ad6c94062
lauratoribio <lsancheztoribio@hotmail.com>
2021-11-21 11:58:11 +0100

    Iteracion#2

---
+++ iteracion2.js
@@ -0,0 +1,13 @@
+const character = {name: 'Jack Sparrow', age: 10};
+//no se puede cambiar el valor porque es una constante
+
+
+let firstName = 'Jon'; 
+let lastName = 'Snow'; 
+let age = 24; 
+console.log("Soy " + firstName + " " + lastName + ", " + "tengo " + age + " años y me gustan los lobos");
+
+const toy1 = {name: 'Buss myYear', price: 19};
+const toy2 = {name: 'Rallo mcKing', price: 29};
+let totalPrice = toy1["price"] + toy2["price"];
+console.log(totalPrice); 
