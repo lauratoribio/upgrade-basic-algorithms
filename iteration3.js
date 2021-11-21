846854f55fce9f300d4c516efca3ec0a71f1c00e
lauratoribio <lsancheztoribio@hotmail.com>
2021-11-21 12:17:01 +0100

    Iteration#3

---
+++ iteration3.js
@@ -0,0 +1,24 @@
+//1.1 Multiplica 10 por 5 y muestra el resultado mediante alert.
+let num1 = 10;
+let num2 = 5;
+alert(num1 * num2);
+
+//1.2 Divide 10 por 2 y muestra el resultado en un alert.
+let num3 = 2;
+alert(num1 / num3);
+
+//1.3 Muestra mediante un alert el resto de dividir 15 por 9.
+let num4 = 15;
+let num5 = 9;
+alert(num4 / num5);
+
+//1.4 Usa el correcto operador de asignación que resultará en x = 15, 
+let y = 10;
+let z = 5;
+let x = y + z;
+
+
+//1.5 Usa el correcto operador de asignación que resultará en x = 50,
+let y = 10;
+let z = 5;
+let x = y * z;
